c571ae0acaf9cdbf9a56902a0ec0c00c69dad622
bjakkula <bjakkula@LIN80016387.corp.capgemini.com>
2019-02-04 15:24:06 +0530

    test

--- src/myPack/Hello.java
+++ src/myPack/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("Eclipse comments");
 		for(int i=0;i<=5;i++)
 		{
-			for(int j=o;j<=i;j++)
+			for(int j=0;j<=i;j++)
 			{
 				System.out.print("*");
 			}
